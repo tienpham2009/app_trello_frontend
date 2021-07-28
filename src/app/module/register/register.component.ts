9eea4ad4c79aad8ab254f1646b9f578a8b0d8565
Duongqua36 <chuduong0306@gmail.com>
2021-07-28 21:03:39 +0700

    update

--- src/app/module/register/register.component.ts
+++ src/app/module/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -7,20 +7,21 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-formRegister!:FormGroup
+  formRegister!: FormGroup
 
   constructor(private fb: FormBuilder,
-              ) { }
+  ) {
+  }
 
   ngOnInit(): void {
-  this.formRegister= this.fb.group({
-    name:['',Validators.required],
-      email: ['',Validators.required],
-      phone:['',Validators.pattern('(84|0[3|5|7|8|9])+([0-9]{8})') ],
-      password: ['',Validators.required],
-      password_confirmation : ['',Validators.required]
-    },
-    {validator: this.ConfirmedValidator('password', 'password_confirmation')})
+    this.formRegister = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', Validators.required],
+        phone: ['', Validators.pattern('(84|0[3|5|7|8|9])+([0-9]{8})')],
+        password: ['', Validators.required],
+        password_confirmation: ['', Validators.required]
+      },
+      {validator: this.ConfirmedValidator('password', 'password_confirmation')})
   }
 
   ConfirmedValidator(controlName: string, matchingControlName: string) {
@@ -37,7 +38,8 @@ formRegister!:FormGroup
       }
     }
   }
-  submit(){
+
+  submit() {
     let userData = this.formRegister?.value
     console.log(userData)
     // this.authService.register(userData).subscribe(res => {
@@ -45,20 +47,24 @@ formRegister!:FormGroup
     //
     // })
   }
-  get name(){
+
+  get name() {
     return this.formRegister?.get('name')
   }
-  get email(){
+
+  get email() {
     return this.formRegister?.get('email')
   }
-  get phone(){
+
+  get phone() {
     return this.formRegister?.get('phone')
   }
 
-  get password(){
+  get password() {
     return this.formRegister?.get('password')
   }
-  get password_confirmation(){
+
+  get password_confirmation() {
     return this.formRegister?.get('password_confirmation')
   }
 
