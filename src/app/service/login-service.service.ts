a4a40f74bb998a36b054f9e9089fbde78d0a08a8
tienpham2009 <tiendx2009@gmail.com>
2021-07-28 18:06:33 +0700

    login frontend

--- src/app/service/login-service.service.ts
+++ src/app/service/login-service.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginServiceService {
+export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  checkLogin(data: any) : Observable<any>{
+    return this.http.post(`${environment.url}` , data)
+  }
+
+  isLogin(){
+    return localStorage.getItem('token');
+  }
+
 }
