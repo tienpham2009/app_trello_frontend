ba10c03b61d367dc32800721427e7631c55e1e7c
tienpham2009 <tiendx2009@gmail.com>
2021-07-28 10:58:16 +0700

    tao thu muc service

--- src/app/module/master/master-routing.module.ts
+++ src/app/module/master/master-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './body/board/board.component';
+import { HomeComponent } from './body/home/home.component';
 
 const routes: Routes = [
-  
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'board',
+    component: BoardComponent,
+  }
 ];
 
 @NgModule({
