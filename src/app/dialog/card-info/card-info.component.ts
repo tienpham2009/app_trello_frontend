e024eb1a4f3320011751f3bcf6cad60e3eea9883
Duongqua36 <chuduong0306@gmail.com>
2021-08-05 13:44:10 +0700

    pull

--- src/app/dialog/card-info/card-info.component.ts
+++ src/app/dialog/card-info/card-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CardService } from 'src/app/service/card.service';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-card-info',
@@ -8,25 +9,44 @@ import { CardService } from 'src/app/service/card.service';
   styleUrls: ['./card-info.component.css'],
 })
 export class CardInfoComponent implements OnInit {
-  
+  formEdit!: FormGroup;
   card: any;
 
+  hidden:boolean =true;
   constructor(
+    private fb:FormBuilder,
     public dialogRef: MatDialogRef<CardInfoComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private cardService: CardService
   ) {}
 
   ngOnInit(): void {
+    this.formEdit=this.fb.group({
+      content:[]
+    })
     let card_id = this.data.number;
-    this.cardService.getCard(card_id).subscribe((res) => {
-      this.card = res.card;
-      console.log(this.card);
-      
-    });
+    this.getCardById(card_id)
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  submit(){
+  let data = this.formEdit?.value;
+    console.log(data)
+  }
+
+  setHidden(){
+    this.hidden=!this.hidden;
+    console.log(this.hidden)
+  }
+
+  getCardById(id:number ){
+    console.log(id)
+    this.cardService.getCardById(id).subscribe((res) => {
+      this.card = res.card;
+      console.log(this.card);
+    });
   }
 }
--- src/app/service/card.service.ts
+++ src/app/service/card.service.ts
@@ -19,12 +19,12 @@ export class CardService {
     };
   }
 
-  getCard(card_id: any): Observable<any> {
+  getCardById(card_id: any): Observable<any> {
     return this.http.get<any>(`${environment.apiUrl}/card/${card_id}/get` , this.setHeader())
   }
 
   storeCard(dataCard:any):Observable<any>{
     return this.http.post<any>(`${environment.apiUrl}/list/changeTitle` , dataCard , this.setHeader())
   }
-  
+
 }
